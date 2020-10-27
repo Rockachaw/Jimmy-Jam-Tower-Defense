99278a9ae9107c9c5a19680354c1bdedf6d5597b
Jacob-Wiedemeier <jacobw-github@jandjw.com>
2020-10-26 22:21:03 -0500

    First real push
    - Added waypoints for enemies
    - Added enemies
    - Added wave spawner
    - Added tower pedestal
    -Organized files
    
    I accidentally made a second scene called MainScene (as opposed to SampleScene). I've been working in MainScene because I can't delete either of them now, but I think the only difference is the layout of objects placed in the editor. They have access to all the same scripts and such.

---
+++ Assets/Scripts/EnemyTypeOne.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyTypeOne : MonoBehaviour
+{
+    public float speed = 11f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.04f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public GameObject towerPrefab;
+
+    private GameObject tower = null;
+
+    public void CreateTower()
+    {
+        tower = (GameObject)Instantiate(towerPrefab, gameObject.transform.position, gameObject.transform.rotation);
+    }
+    public void DestroyTower()
+    {
+        try
+        {
+            tower = null; ;
+        }
+        catch
+        {
+            Debug.LogError("Failed to destroy towerObject.");
+        }
+    }
+}
