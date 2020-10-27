4734b7707d945aad6c91ac1def2561dba064bce5
Rockachaw <amazinghdh@gmail.com>
2020-10-27 02:01:00 -0500

    Health bar added
    Made a health bar for the player and a function to damage the player. Could not find a way to call the function when an enemy hits the last node

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+    private float playerHealth;
+
+    private void Start()
+    {
+        playerHealth = 1.0f;
+        healthBar.SetSize(playerHealth);
+        LowerPlayerHealth(0.2f);
+    }
+    public void LowerPlayerHealth(float damage)
+    {
+        if (playerHealth > 0) {
+            playerHealth -= damage;
+            healthBar.SetSize(playerHealth);
+        }
+    }
+
+
+}
