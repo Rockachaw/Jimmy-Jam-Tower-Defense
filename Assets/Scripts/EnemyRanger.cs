dd973a4f8fc0f66e451141c02ffa4d6d8cfc614b
Jacob-Wiedemeier <jacobw-github@jandjw.com>
2020-10-27 22:37:42 -0500

    BEHOLD! A MAN!
    + Added heart system for player life
    + Upgraded EnemyTypeOne into Ranger, a basic enemy with low stats and a functioning walking animation.
    
    - Deleted health bar, might need to retrieve to recycle mechanic later
    
    * Game does not end if player runs out of lives

---
+++ Assets/Scripts/EnemyRanger.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyRanger : MonoBehaviour
+{
+    public float speed = 11f;
+    public float health = 10.0f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private SpriteRenderer enemySpriteRenderer;
+    private Sprite[] enemySpriteArray;
+
+    //Notes which frame of the animation the enemy is on
+    private int animationCycleIndex = 0;
+
+    //Animation is a 3 frame cycle and the middle fram is the neutral pose, so we want to
+    //  animate in the order 1-2-3-2-1-2-3-2...
+    private bool animationCycleReversing = false;
+
+    //Determines framerate of animation
+    private float animationCycleCooldown = 0.2f;
+
+    void Start()
+    {
+        enemySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        enemySpriteArray = Resources.LoadAll<Sprite>("Male 03-1");
+        enemySpriteRenderer.sprite = enemySpriteArray[0];
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        dir = dir.normalized;
+        
+        //Move
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        
+        //Reached waypoint, find next one.
+        if (Vector2.Distance(transform.position, target.position) <= 0.04f)
+        {
+            GetNextWaypoint();
+        }
+
+        //Handle animation
+        if(Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            //Moving more x than y
+            if(dir.x > 0)
+            {
+                //Moving right
+                enemySpriteRenderer.sprite = enemySpriteArray[6 + animationCycleIndex];
+            }
+            else
+            {
+                //Moving left
+                enemySpriteRenderer.sprite = enemySpriteArray[3 + animationCycleIndex];
+            }
+        }
+        else
+        {
+            //Moving more y than x
+            if(dir.y > 0)
+            {
+                //Moving up
+                enemySpriteRenderer.sprite = enemySpriteArray[9 + animationCycleIndex];
+            }
+            else
+            {
+                //Technically default case if not moving, but enemies should always be moving.
+                //Moving down
+                enemySpriteRenderer.sprite = enemySpriteArray[0 + animationCycleIndex];
+            }
+        }
+
+        //Progress animation cycle
+        animationCycleCooldown -= Time.deltaTime;
+        if (animationCycleCooldown <= 0)
+        {
+            animationCycleCooldown = 0.2f;
+
+            if (animationCycleReversing)
+            {
+                animationCycleIndex--;
+            }
+            else
+            {
+                animationCycleIndex++;
+            }
+
+            if (animationCycleIndex != 1)
+            {
+                animationCycleReversing = !animationCycleReversing;
+            }
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            GameObject healthReference = GameObject.Find("Health");
+            HeartController healthScript = (HeartController)healthReference.GetComponent(typeof(HeartController));
+            healthScript.LoseHealth();
+
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+
+    public void GetDamaged(float damageTaken)
+    {
+        health -= damageTaken;
+        if(health <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HeartController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartController : MonoBehaviour
+{
+    public GameObject heartSprite;
+    public int Health = 4;
+    public Vector3 heartPosition;
+    public float distanceBetweenHearts;
+
+    private GameObject[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hearts = new GameObject[Health];
+        heartSprite.transform.position = heartPosition;
+        Transform adjustedTransform = heartSprite.transform;
+
+        for(int i = 0; i < Health; i++)
+        {
+            hearts[i] = (GameObject)Instantiate(heartSprite, adjustedTransform);
+            adjustedTransform.Translate(distanceBetweenHearts, 0.0f, 0.0f);
+        }
+    }
+
+    public void LoseHealth()
+    {
+        for(int i = hearts.Length-1; i >= 0; i--)
+        {
+            if(hearts[i] != null)
+            {
+                Destroy(hearts[i]);
+                return;
+            }
+        }
+        Debug.LogError("Out of health, game over");
+    }
+}
